6de469490d3b22be883ddaee7ec04f91adddf7b2
Seth Rojas <sethrojas24@icloud.com>
2026-05-20 16:09:00 -0700

    Added the buttons for styles and its alr

--- components/home/About.tsx
+++ components/home/About.tsx
@@ -4,6 +4,9 @@ import { ScrollView } from "@nandorojo/anchor";
 import { View, Image, StyleSheet, Text } from "react-native";
 import { InlineAccentLink } from "../AccentLink";
 import { EMAIL_LINK, GITHUB_LINK, LINKEDIN_LINK } from "@/config/config";
+import CircleIconButton from "../landing/social_icon";
+import { openInNewTab } from "@/utils/linking";
+import { openURL } from "expo-linking";
 
 export default function About() {
   return (
@@ -18,10 +21,40 @@ export default function About() {
           <Text style={styles.blurb}>{ABOUT_INTRO}</Text>
         </View>
 
-        <Text style={styles.blurb}>
+        {/* <Text style={styles.blurb}>
           You can reach me on <InlineAccentLink text="LinkedIn" href={LINKEDIN_LINK} />, <InlineAccentLink text="GitHub" href={GITHUB_LINK} />, 
           and <InlineAccentLink text="Email" href={EMAIL_LINK} />
-        </Text>
+        </Text> */}
+
+        <View style={styles.row}>
+            <CircleIconButton
+                icon={{ pack: "ion", name: "logo-github", color: "#6F42C1" }}
+                accessibilityLabel="Open GitHub"
+                style={styles.githubBtn}
+                onPress={() => openInNewTab(GITHUB_LINK)}
+            />
+            <CircleIconButton
+                icon={{ pack: "ion", name: "logo-linkedin", color: "#0A66C2" }}
+                accessibilityLabel="Open LinkedIn"
+                style={styles.linkedinBtn}
+                onPress={() => openInNewTab(LINKEDIN_LINK)}
+            />
+            <CircleIconButton
+                icon={{ pack: "ion", name: "mail-outline", color: "#EA4335" }}
+                accessibilityLabel="Send Email"
+                style={styles.emailBtn}
+                onPress={() => openURL("mailto:skrojas@arizona.edu")}
+            />
+            <CircleIconButton
+                icon={{ pack: "ion", name: "document-outline", color: "grey" }}
+                accessibilityLabel="Download Resume"
+                onPress={() => openInNewTab("/resume/swe_resume.pdf")}
+                style={{
+                    borderColor: "#E9E2FF",
+                    backgroundColor: "#F3F0FF",
+                }}
+            />
+        </View>
       </ScrollView>
     </View>
   );
@@ -45,5 +78,23 @@ const styles = StyleSheet.create({
     fontSize: FONT_SIZE.md,
     lineHeight: 24,
     fontFamily: "Inter_400Regular"
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 40,
+    marginVertical: 8,
+  },
+  githubBtn: {
+      borderColor: "#E9E2FF",
+      backgroundColor: "#F3F0FF",
+  },
+  linkedinBtn: {
+      borderColor: "#D6EAFB",
+      backgroundColor: "#E6F2FB",
+  },
+  emailBtn: {
+      borderColor: "#FFE6E4",
+      backgroundColor: "#FFF1F0",
   },
 });
